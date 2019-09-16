1e3e77d492d97c8f86acdd762cca77843bec221d
danikleonov <47587031+danikleonov@users.noreply.github.com>
2019-09-16 19:12:55 +0300

    Add files via upload

--- BusinessTrip/src/Runner.java
+++ BusinessTrip/src/Runner.java
@@ -1,12 +1,12 @@
-import by.gsu.pmslab.BusinessTrip;
-import by.gsu.pmslab.Converter;
+import by.gsu.mslab.BusinessTrip;
+import by.gsu.mslab.Converter;
 
 public class Runner {
     public static void main(String[] args) {
         BusinessTrip [] businessTrips = {
                 new BusinessTrip("Leonov Daniil", 2550 , 10),
                 new BusinessTrip("Socolov Evgeniy", 3820, 12),
-                new BusinessTrip(null,0,0),
+                null,
                 new BusinessTrip("Vinogradov Pavel",5500,4),
                 new BusinessTrip("Kiselev Aleksei",4230,19),
                 new BusinessTrip("Petrov Petr", 1300,21),
@@ -14,7 +14,8 @@ public class Runner {
         };
 
         for(BusinessTrip item : businessTrips){
-            item.show();
+            if (item != null)
+                item.show();
             System.out.println();
         }
 
@@ -24,20 +25,24 @@ public class Runner {
         System.out.println();
 
         for (BusinessTrip item : businessTrips) {
-            System.out.println(item);
+            if (item != null)
+                System.out.println(item);
             System.out.println();
         }
 
         int total_sum = 0;
         for(BusinessTrip item : businessTrips){
-            total_sum += item.getTotal();
+            if (item != null)
+                total_sum += item.getTotal();
         }
         System.out.println("Total expenses = "+Converter.convert(total_sum,100,2));
 
         BusinessTrip max_total = businessTrips[0];
         for(BusinessTrip item : businessTrips){
-            if(item.getTotal() > max_total.getTotal())
-                max_total = item;
+            if (item != null) {
+                if (item.getTotal() > max_total.getTotal())
+                    max_total = item;
+            }
         }
         System.out.println("Max total account - " + max_total.getAccount());
 
