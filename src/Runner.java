5310d53292a6d5f808f73c0d7893c9ccd9da6e5c
danikleonov <47587031+danikleonov@users.noreply.github.com>
2019-09-18 15:22:47 +0200

    Changes

--- src/Runner.java
+++ src/Runner.java
@@ -2,14 +2,19 @@ import by.gsu.mslab.Tribe;
 
 import java.io.*;
 import java.util.Scanner;
+import java.io.FilenameFilter;
 
 public class Runner {
     public static void main(String[] args) throws IOException, ClassNotFoundException {
 
         char answer;
-        final int TRIBES_NUMBER = 1;
+        final int TRIBES_NUMBER = 5;
         Tribe[] tribes = new Tribe[TRIBES_NUMBER];
         String filename;
+
+        int summNumber = 0;
+        int doFireCount = 0;
+
 
 
         System.out.println("Write to new file(w) or Read info from file(r)?");
@@ -19,8 +24,8 @@ public class Runner {
 
         if (answer == 'w'){
             System.out.println("---------------------------");
-            System.out.println("Directory, where file will be created: /src/example.txt");
-            System.out.println("Enter filename: ");
+            System.out.println("Directory, where file will be created: /home/alwayswannafly/lab4/src/");
+            System.out.print("Enter filename: ");
             filename = scan.next();
             System.out.println("---------------------------");
 
@@ -32,39 +37,63 @@ public class Runner {
 
             FileOutputStream fileOutputStream = new FileOutputStream("src/"+filename+".txt");
             ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
-            Tribe tribe = new Tribe();
 
-            System.out.println("---------------------------");
-            System.out.println("Enter information about TRIBES");
-            System.out.println("---------------------------");
+            for (int i = 0; i < TRIBES_NUMBER; i++){
+                Tribe tribe = new Tribe();
 
-            System.out.print("Enter name: ");
-            tribe.setName(scan.next());
+                System.out.println("---------------------------");
+                System.out.println("Enter information about TRIBES");
+                System.out.println("---------------------------");
 
-            System.out.print("Enter number: ");
-            tribe.setNumber(scan.nextInt());
+                System.out.print("Enter name: ");
+                tribe.setName(scan.next());
 
-            System.out.print("Enter doFire?: ");
-            tribe.setDoFire(scan.next());
+                System.out.print("Enter number: ");
+                tribe.setNumber(scan.nextInt());
 
-            objectOutputStream.writeObject(tribe);
+                System.out.print("Enter doFire?: ");
+                tribe.setDoFire(scan.next());
 
-            System.out.println("---------------------------");
-            System.out.println("Info has been written into "+"src/"+filename+".txt");
-            System.out.println("---------------------------");
+                objectOutputStream.writeObject(tribe);
 
+                System.out.println("---------------------------");
+                System.out.println("Info has been written into "+"src/"+filename+".txt");
+                System.out.println("---------------------------");
+
+            }
         }
 
 
         if (answer == 'r'){
 
             System.out.println("---------------------------");
-            System.out.println("Directory where files are saved: /src/example.txt");
-            System.out.println("Enter filename: ");
+            System.out.println("Directory where files are saved: /home/alwayswannafly/lab4/src/");
+            System.out.println("We found these .txt files here: \n");
+
+            File folder = new File("/home/alwayswannafly/lab4/src/");
+
+            String[] files = folder.list(new FilenameFilter() {
+
+                @Override public boolean accept(File folder, String name) {
+                    return name.endsWith(".txt");
+                }
+
+            });
+
+            for ( String fileName : files ) {
+                System.out.println(fileName);
+            }
+
+
+
+          ;
+
+            System.out.println("\n---------------------------");
+            System.out.print("Enter filename: ");
 
             filename = scan.next();
 
-            if (new File(filename).exists() == true) {
+            if (!(new File("src/"+filename+".txt").exists())) {
                 System.out.println("No such file in dir");
                 System.exit(1);
 
@@ -73,10 +102,24 @@ public class Runner {
             FileInputStream fileInputStream = new FileInputStream("src/"+filename+".txt");
             ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream);
 
+            System.out.println("\n---------------------------");
+            System.out.println("Acquired information: ");
+            System.out.println("---------------------------");
+
             for (int i = 0; i < TRIBES_NUMBER; i++){
                 tribes[i] = (Tribe) objectInputStream.readObject();
                 System.out.println(tribes[i].toString());
+
+                summNumber += tribes[i].getNumber();
+
+                if(tribes[i].getDoFire().equals("yes")){
+                    doFireCount++;
+                }
+
             }
+
+            System.out.println("\nTotal people in tribes: "+summNumber);
+            System.out.println("Number of tribes familiar with fire: "+doFireCount);
 
         }
 
